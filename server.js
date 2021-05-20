dc70cf83f22d5d67346c6cf51bb996df2c24e7a7
Shahid Rahmat <76436222+ShahidRahmat@users.noreply.github.com>
2021-05-21 02:12:54 +0800

    Update server.js

--- server.js
+++ server.js
@@ -15,6 +15,12 @@ client.on("message", message => {
 if (message.content === '-Hello') {
   message.channel.send("Hello to you too")
 }
+if (message.content === '-Coursemology') {
+  message.channel.send('Coursemology link: https://coursemology.org/')
+}
+if (message.content === '-MeL') {
+  message.channel.send('MeL NP: https://mel.np.edu.sg/ultra/course')
+}
 if (message.content === '-API') {
   message.channel.send('Discord JS Documentation: https://discord.js.org/#/docs/main/stable/general/welcome')
 }
@@ -46,6 +52,37 @@ if ((message.content.startsWith('-Avatar')) && (message.mentions.users.first)) {
 if (message.content === '-CM') {
   message.channel.send("Tuesday: 11am-1pm")
   message.channel.send("Friday: 10am-12pm")
+}
+if (message.content === '-DSF') {
+  message.channel.send("Tuesday: 9am-11am")
+}
+if (message.content === '-PRG') {
+  message.channel.send("Monday: 12pm-3pm")
+  message.channel.send("Friday: 2pm-4pm")
+}
+if (message.content === '-DP') {
+  message.channel.send("Thursday: 11am-1pm")
+}
+if (message.content === '-CSF') {
+  message.channel.send("Thursday: 2pm-4pm")
+}
+if (message.content === '-FP') {
+  message.channel.send("Monday: 4pm-6pm")
+}
+if (message.content === '-ALL') {
+  message.channel.send("PRG: \nMonday: 12pm-3pm \nFriday: 2pm-4pm  \nFP: \nMonday: 4pm-6pm \nDSF: \nTuesday: 9am-11am \nCM: \nTuesday: 11am-1pm \nFriday: 10am-12pm \nDP: \nThursday: 11am-1pm \nCSF: \nThursday: 2pm-4pm")
+}
+if ((message.author.id === '353177950033674240') && (message.content === "-COMMS")) {
+    message.channel.send("Wednesday: 1pm-4pm")
+}
+if ((!message.author.id === '353177950033674240') && (message.content === "-COMMS")) {
+    message.channel.send("Wednesday: 9am-12pm")
+}
+if ((message.author.id === '353177950033674240') && (message.content === "-HW")) {
+    message.channel.send("Wednesday: 8am-10am")
+  }
+if (!(message.author.id === '353177950033674240') && (message.content === "-HW")) {
+    message.channel.send("Wednesday: 1pm-3pm")
 }
 })
 
